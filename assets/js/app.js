874b89dfeb97e898f5c2760827b8bcb11d008d10
Michael ZHANG <geek.michael@live.com>
2017-02-22 07:38:17 +0800

    Updated with customized map pin, and added map marker function

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,6 +26,8 @@ var Model = {
             lng: 115.263656
         }
     ],
+
+    infoboxHTML: '<div class="marker-info"><h2 class="marker-title">%title%</h2><div class="marker-content">Loading ...</div></div>',
     /* Set Google map type by following https://developers.google.com/maps/documentation/javascript/maptypes
       roadmap: displays the default road map view. This is the default map type.
       satellite: displays Google Earth satellite images
@@ -36,8 +38,26 @@ var Model = {
 };
 
 var viewModel = {
+
     locations: ko.observableArray(Model.locations),
+
+    // Get the pre-defined coordinate as map centre
+    getMapCenter: function() {
+        var mapCenter = Model.mapCenter();
+        return mapCenter[0];
+    },
+    // Update map centre with new coordinate
+    setMapCenter: function(lat, lng) {
+        console.log("Clicked");
+        Model.mapCenter([{lat, lng}]);
+    },
+
+    getMapTypeId: function(){
+        return Model.mapTypeId;
+    },
+
     init: function(){
+        this.currentLocation = ko.observable(this.locations()[0]);
         this.initMap();
         //this.initMarker();
         this.displayLocations();
@@ -46,10 +66,17 @@ var viewModel = {
     initMap: function(){
         var mapOptions = {
           zoom: 10,
+          disableDefaultUI: true,
           center: this.getMapCenter(),
           mapTypeId: this.getMapTypeId()
         };
         this.map = new google.maps.Map(Model.mapDiv, mapOptions);
+        this.infobox = new google.maps.InfoWindow({
+            content: "Loading ...."
+        });
+        this.latLngBounds = new google.maps.LatLngBounds();
+        // An array to store all location markers
+        this.markers = [];
     },
     // Add the first marker on the map
     initMarker: function() {
@@ -59,55 +86,56 @@ var viewModel = {
         });
     },
 
+    updateInfobox: function(marker) {
+        this.markerInfo = Model.infoboxHTML.replace("%title%", marker.title);
+    },
+
     // Iterate over locations array and create markers on the map
     displayLocations: function(){
         //Set the map bounds and zoom level according to markers position
         var i, location, latLng, marker;
-
-        var map = this.map,
-            infobox = new google.maps.InfoWindow({
-                content: "Loading ...."
-            }),
-            locationLen = this.locations().length,
-            latLngBounds = new google.maps.LatLngBounds();
+        var map = this.map;
+        var locationLen = this.locations().length;
         // Use for instead of Array.forEach
         // Performance comparison http://jsperf.com/fast-array-foreach
         for ( i = 0; i < locationLen; i += 1){
             location = this.locations()[i];
             latLng = new google.maps.LatLng(location.lat, location.lng);
-            latLngBounds.extend(latLng);
+            this.latLngBounds.extend(latLng);
             marker = new google.maps.Marker({
               position: latLng,
               map: map,
-              title: location.name
+              title: location.name,
+              infoWindowIndex: i
             });
+            //marker.setMap(this.map);
+            this.markers.push(marker);
 
-            // Centering map on marker click
-            google.maps.event.addListener(marker, 'click', function(){
-                infobox.setContent(location.name);
-                infobox.open(map, this);
-                map.setCenter(this.getPosition());
-            });
-
-            // Centering and fitting all markers on the screen
-            map.fitBounds(latLngBounds);
+            google.maps.event.addListener(marker, 'click', function() {
+                return function() {
+                    viewModel.displayMarker(marker, this.markerInfo);
+                }
+            }(i));
         }
+        // Centering and fitting all markers on the screen
+        map.fitBounds(this.latLngBounds);
     },
-
-    // Get the pre-defined coordinate as map centre
-    getMapCenter: function() {
-        var mapCenter = Model.mapCenter();
-        return mapCenter[0];
+    displayMarker: function(marker, markerInfo) {
+        this.updateInfobox(marker);
+        this.infobox.close();
+        console.log(this.markerInfo);
+        // Reset all markers with default icons
+        this.resetMarkers();
+        this.infobox.setContent(this.markerInfo);
+        this.infobox.open(this.map, marker);
+        //this.map.setCenter(this.getPosition());
     },
-    // Update map centre with new coordinate
-    setMapCenter: function(lat, lng) {
-        Model.mapCenter([{lat, lng}]);
-    },
-
-    getMapTypeId: function(){
-        return Model.mapTypeId;
+    resetMarkers: function() {
+        var i, markerLen = this.markers.length;
+        for (i = 0; i < markerLen; i += 1) {
+            this.markers[i].setIcon('assets/images/map-pin-default.png');
+        }
     }
-
 };
 // ko.bindingHandlers.googlemap = {
 //     init: function(element, valueAccessor) {
